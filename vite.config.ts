c404a5837aa204d3229cc01974ae52f2f4c909ef
saudiatransportation2026 <sauditaxiservices@gmail.com>
2026-06-05 15:46:34 +0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsConfigPaths()
+  ],
+  build: {
+    outDir: "dist/client",
+  },
+});
